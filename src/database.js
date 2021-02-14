62c1e8f5e5cb9a43d4d0d1b844c5e255f09d464e
gon <gonzalo_092@yahoo.com>
2021-02-13 21:30:31 -0300

    ddd

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const config = require("./config");
+//const config = require("./config");
 
-const MONGODB_URI = `mongodb://${config.MONGODB_HOST}/${config.MONGODB_DATABASE}`;
+//const MONGODB_URI = `mongodb://${config.MONGODB_HOST}/${config.MONGODB_DATABASE}`;
 
 mongoose
   .connect('mongodb+srv://gonzaloUser:rocanroles092@cluster0.z2hxo.mongodb.net/notes-app?retryWrites=true&w=majority', {
